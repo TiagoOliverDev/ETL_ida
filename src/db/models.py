34efd5650a19da97f8d32107bc4215cf283064c6
TiagoOliverDev <paulloholiveira77@gmail.com>
2025-08-01 15:38:46 -0300

    ref: ajuste no model

--- src/db/models.py
+++ src/db/models.py
@@ -2,6 +2,16 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from src.db.database import Base
 
+
+class DimVariavel(Base):
+    __tablename__ = 'dim_variavel'
+
+    id_variavel = Column(Integer, primary_key=True, autoincrement=True)
+    nome_variavel = Column(String(255), nullable=False)
+
+    fatos = relationship("FatoIndicador", back_populates="variavel")
+
+
 class DimTempo(Base):
     __tablename__ = 'dim_tempo'
 
@@ -10,7 +20,7 @@ class DimTempo(Base):
     mes = Column(Integer, nullable=False)
     mes_ano = Column(String(7), nullable=False)
 
-    fatos = relationship("FatoIda", back_populates="tempo")
+    fatos = relationship("FatoIndicador", back_populates="tempo")
 
 
 class DimGrupoEconomico(Base):
@@ -19,7 +29,7 @@ class DimGrupoEconomico(Base):
     id_grupo_economico = Column(Integer, primary_key=True, autoincrement=True)
     nome_grupo = Column(String(255), nullable=False)
 
-    fatos = relationship("FatoIda", back_populates="grupo_economico")
+    fatos = relationship("FatoIndicador", back_populates="grupo_economico")
 
 
 class DimServico(Base):
@@ -28,18 +38,20 @@ class DimServico(Base):
     id_servico = Column(Integer, primary_key=True, autoincrement=True)
     nome_servico = Column(String(50), nullable=False)
 
-    fatos = relationship("FatoIda", back_populates="servico")
+    fatos = relationship("FatoIndicador", back_populates="servico")
 
 
-class FatoIda(Base):
-    __tablename__ = 'fato_ida'
+class FatoIndicador(Base):
+    __tablename__ = 'fato_indicador'
 
     id_fato = Column(Integer, primary_key=True, autoincrement=True)
     id_tempo = Column(Integer, ForeignKey('dim_tempo.id_tempo'), nullable=False)
     id_grupo_economico = Column(Integer, ForeignKey('dim_grupo_economico.id_grupo_economico'), nullable=False)
     id_servico = Column(Integer, ForeignKey('dim_servico.id_servico'), nullable=False)
-    valor_ida = Column(Float, nullable=False)
+    id_variavel = Column(Integer, ForeignKey('dim_variavel.id_variavel'), nullable=False)
+    valor = Column(Float, nullable=False)
 
     tempo = relationship("DimTempo", back_populates="fatos")
     grupo_economico = relationship("DimGrupoEconomico", back_populates="fatos")
     servico = relationship("DimServico", back_populates="fatos")
+    variavel = relationship("DimVariavel", back_populates="fatos")
